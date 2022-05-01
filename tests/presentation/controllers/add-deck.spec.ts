6659f30a4cc9b4a2b96df1705d030786f8000bbf
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-01 13:32:11 -0300

    test: ensure AddDeckController returns 400 if Validation fails

---
+++ tests/presentation/controllers/add-deck.spec.ts
@@ -0,0 +1,30 @@
+import faker from '@faker-js/faker'
+
+import { AddDeckController } from '@/presentation/controllers'
+import { ValidationSpy } from '@/tests/presentation/mocks'
+import { badRequest } from '@/presentation/helpers'
+
+const mockRequest = (): AddDeckController.Request => ({
+  title: faker.random.words(),
+  language: faker.random.word()
+})
+
+type SutTypes = {
+  sut: AddDeckController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = new AddDeckController(validationSpy)
+  return { sut, validationSpy }
+}
+
+describe('AddDeck Controller', () => {
+  it('should return 400 if Validation fails', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const res = await sut.perform(mockRequest())
+    expect(res).toEqual(badRequest(validationSpy.error))
+  })
+})
