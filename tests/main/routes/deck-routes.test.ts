8f7ea57414d9ff01cde2116265b331351f9ee5bb
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-02 04:11:41 -0300

    test: ensure add deck route returns 204 on add deck

---
+++ tests/main/routes/deck-routes.test.ts
@@ -0,0 +1,62 @@
+import { Express } from 'express'
+import request from 'supertest'
+import { Collection, ObjectId } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+
+import { setupApp } from '@/main/config/app'
+import { MongoHelper } from '@/infra/db'
+import env from '@/main/config/env'
+
+let accountCollection: Collection
+let deckCollection: Collection
+let app: Express
+
+const mockAccount = async (): Promise<{
+  accessToken: string
+  id: ObjectId
+}> => {
+  const res = await accountCollection.insertOne({
+    email: 'any_email@mail.com',
+    username: 'any_username',
+    password: 'any_password',
+    role: 'admin'
+  })
+  const accessToken = sign({ id: res.insertedId }, env.jwtSecret)
+  await accountCollection.updateOne(
+    { _id: res.insertedId },
+    { $set: { accessToken } }
+  )
+  return { accessToken, id: res.insertedId }
+}
+
+describe('Deck Routes', () => {
+  beforeAll(async () => {
+    app = await setupApp()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+    deckCollection = MongoHelper.getCollection('decks')
+    await deckCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('POST /decks', () => {
+    it('should return 204 on add deck', async () => {
+      const { accessToken } = await mockAccount()
+      await request(app)
+        .post('/api/decks')
+        .send({
+          title: 'any_title',
+          language: 'any_language'
+        })
+        .set('x-access-token', accessToken)
+        .expect(204)
+    })
+  })
+})
