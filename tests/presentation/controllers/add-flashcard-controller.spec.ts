6916c59e4f960f1f1cbee6ce4865cb47264a44a7
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-02 18:57:26 -0300

    test: ensure AddFlashcardController returns 400 if Validation fails

---
+++ tests/presentation/controllers/add-flashcard-controller.spec.ts
@@ -0,0 +1,45 @@
+import faker from '@faker-js/faker'
+
+import { AddFlashcardController } from '@/presentation/controllers'
+import { ValidationSpy, AddFlashcardSpy } from '@/tests/presentation/mocks'
+import { badRequest } from '@/presentation/helpers'
+
+const mockRequest = (): AddFlashcardController.Request => ({
+  deckId: faker.datatype.uuid(),
+  accountId: faker.datatype.uuid(),
+  flashcard: {
+    front: {
+      content: faker.random.words(),
+      howToRead: faker.random.words()
+    },
+    back: {
+      content: faker.random.words(),
+      glossary: {
+        words: [faker.random.word()],
+        meanings: [faker.random.word()]
+      }
+    }
+  }
+})
+
+type SutTypes = {
+  sut: AddFlashcardController
+  validationSpy: ValidationSpy
+  addFlashcardSpy: AddFlashcardSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const addFlashcardSpy = new AddFlashcardSpy()
+  const sut = new AddFlashcardController(validationSpy, addFlashcardSpy)
+  return { sut, validationSpy, addFlashcardSpy }
+}
+
+describe('AddFlashcard Controller', () => {
+  it('should return 400 if Validation fails', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const res = await sut.perform(mockRequest())
+    expect(res).toEqual(badRequest(validationSpy.error))
+  })
+})
--- tests/presentation/mocks/mock-deck.ts
+++ tests/presentation/mocks/mock-deck.ts
@@ -1,9 +1,19 @@
-import { AddDeck } from '@/domain/usecases'
+import { AddDeck, AddFlashcard } from '@/domain/usecases'
 
 export class AddDeckMock implements AddDeck {
   params: AddDeck.Params
 
   async add(params: AddDeck.Params): Promise<AddDeck.Result> {
     this.params = params
+  }
+}
+
+export class AddFlashcardSpy implements AddFlashcard {
+  params: AddFlashcard.Params
+  result: boolean = true
+
+  async add(params: AddFlashcard.Params): Promise<boolean> {
+    this.params = params
+    return this.result
   }
 }
