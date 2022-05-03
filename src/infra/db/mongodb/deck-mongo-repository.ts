dfad6541cc07cecb05554224d1a0610f6e0666c5
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-03 00:53:31 -0300

    feat: ensure DeckMongoRepository adds a flashcard on addFlashcard success

--- src/infra/db/mongodb/deck-mongo-repository.ts
+++ src/infra/db/mongodb/deck-mongo-repository.ts
@@ -1,11 +1,26 @@
-import { AddDeckRepository } from '@/data/contracts'
+import { AddDeckRepository, AddFlashcardRepository } from '@/data/contracts'
 import { MongoHelper } from '@/infra/db'
+import { ObjectId } from 'mongodb'
 
-export class DeckMongoRepository implements AddDeckRepository {
+export class DeckMongoRepository
+  implements AddDeckRepository, AddFlashcardRepository
+{
   async add(
     params: AddDeckRepository.Params
   ): Promise<AddDeckRepository.Result> {
     const deckCollection = MongoHelper.getCollection('decks')
     await deckCollection.insertOne(params)
+  }
+
+  async addFlashcard(
+    params: AddFlashcardRepository.Params
+  ): Promise<AddFlashcardRepository.Result> {
+    const { deckId, flashcard } = params
+    const deckCollection = MongoHelper.getCollection('decks')
+    const result = await deckCollection.updateOne(
+      { _id: new ObjectId(deckId) },
+      { $addToSet: { flashcards: flashcard } }
+    )
+    return result.acknowledged !== null
   }
 }
