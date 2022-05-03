6730fba7c42820b9330033da7872b31a92c08aa5
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-03 19:54:51 -0300

    feat: ensure LoadDecksController returns 500 if LoadDecks throws

--- src/presentation/controllers/load-decks-controller.ts
+++ src/presentation/controllers/load-decks-controller.ts
@@ -1,13 +1,17 @@
 import { Controller, HttpResponse } from '@/presentation/contracts'
 import { LoadDecks } from '@/domain/usecases'
-import { ok, noContent } from '@/presentation/helpers'
+import { ok, noContent, serverError } from '@/presentation/helpers'
 
 export class LoadDecksController implements Controller {
   constructor(private readonly loadDecks: LoadDecks) {}
 
   async perform(request: LoadDecksController.Request): Promise<HttpResponse> {
-    const decks = await this.loadDecks.load(request.accountId)
-    return decks.length ? ok(decks) : noContent()
+    try {
+      const decks = await this.loadDecks.load(request.accountId)
+      return decks.length ? ok(decks) : noContent()
+    } catch (err) {
+      return serverError(err)
+    }
   }
 }
 
