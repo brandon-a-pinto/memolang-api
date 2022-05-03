68862b5970bc1317ab7c6a52ddd161101e09dad1
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-03 00:58:13 -0300

    test: ensure DeckMongoRepository adds a flashcard on addFlashcard success

--- tests/infra/db/mongodb/deck-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/deck-mongo-repository.spec.ts
@@ -1,8 +1,27 @@
 import { Collection } from 'mongodb'
 
 import { MongoHelper, DeckMongoRepository } from '@/infra/db'
-import { mockAddDeckParams } from '@/tests/domain/mocks'
+import {
+  mockAddDeckParams,
+  mockAddFlashcardParams,
+  mockAddAccountParams
+} from '@/tests/domain/mocks'
 
+const mockData = async () => {
+  const insertAccount = await accountCollection.insertOne(
+    mockAddAccountParams()
+  )
+  const { title, language } = mockAddDeckParams()
+  const insertDeck = await deckCollection.insertOne({
+    title,
+    language,
+    ownerId: insertAccount.insertedId
+  })
+  const deck = await deckCollection.findOne({ _id: insertDeck.insertedId })
+  return { deck, id: insertAccount.insertedId }
+}
+
+let accountCollection: Collection
 let deckCollection: Collection
 
 const makeSut = (): DeckMongoRepository => new DeckMongoRepository()
@@ -13,6 +32,8 @@ describe('DeckMongoRepository', () => {
   })
 
   beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
     deckCollection = MongoHelper.getCollection('decks')
     await deckCollection.deleteMany({})
   })
@@ -27,6 +48,22 @@ describe('DeckMongoRepository', () => {
       await sut.add(mockAddDeckParams())
       const count = await deckCollection.countDocuments()
       expect(count).toBe(1)
+    })
+  })
+
+  describe('addFlashcard()', () => {
+    it('should add a flashcard on success', async () => {
+      const sut = makeSut()
+      const { deck, id } = await mockData()
+      const { flashcard } = mockAddFlashcardParams()
+      await sut.addFlashcard({
+        ownerId: id.toHexString(),
+        deckId: deck._id.toHexString(),
+        flashcard
+      })
+      const res = await deckCollection.findOne({ _id: deck._id })
+      expect(res).toBeTruthy()
+      expect(res.flashcards).toEqual([flashcard])
     })
   })
 })
