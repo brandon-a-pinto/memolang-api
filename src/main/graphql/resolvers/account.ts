ebf8db7c680ade08941520a8d78d9d99472809a3
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-11 14:54:50 -0300

    feat: make login resolver use login controller

--- src/main/graphql/resolvers/account.ts
+++ src/main/graphql/resolvers/account.ts
@@ -1,9 +1,9 @@
+import { adaptResolver } from '@/main/adapters'
+import { makeLoginController } from '@/main/factories'
+
 export default {
   Query: {
-    login() {
-      return {
-        accessToken: 'any_token'
-      }
-    }
+    login: async (_parent: any, args: any) =>
+      adaptResolver(makeLoginController(), args)
   }
 }
