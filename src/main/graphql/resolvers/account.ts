1343f32da64de1a022610bc8edd25b7ab72e9108
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-11 15:53:13 -0300

    feat: add signup mutation

--- src/main/graphql/resolvers/account.ts
+++ src/main/graphql/resolvers/account.ts
@@ -1,9 +1,14 @@
 import { adaptResolver } from '@/main/adapters'
-import { makeLoginController } from '@/main/factories'
+import { makeLoginController, makeSignUpController } from '@/main/factories'
 
 export default {
   Query: {
     login: async (_parent: any, args: any) =>
       adaptResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signup: async (_parent: any, args: any) =>
+      adaptResolver(makeSignUpController(), args)
   }
 }
--- src/main/graphql/type-defs/account.ts
+++ src/main/graphql/type-defs/account.ts
@@ -5,6 +5,15 @@ export default gql`
     login(email: String!, password: String!): Account!
   }
 
+  extend type Mutation {
+    signup(
+      email: String!
+      username: String!
+      password: String!
+      passwordConfirmation: String!
+    ): Boolean
+  }
+
   type Account {
     accessToken: String!
   }
