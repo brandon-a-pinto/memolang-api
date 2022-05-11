80bf32610fa4a0f1a9721bc8905162109e406f4e
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-11 14:40:49 -0300

    feat: setup apollo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express'
 
+import setupApolloServer from '@/main/config/apollo-server'
 import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  setupApolloServer(app)
   setupMiddlewares(app)
   setupRoutes(app)
   return app
