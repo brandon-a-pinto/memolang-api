bc946633ca0687e78faf68b2e1ccd9c9c8ca1bfd
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-28 13:09:18 -0300

    docs: add signup api docs

---
+++ src/main/docs/paths/signup-path.ts
@@ -0,0 +1,34 @@
+export const signUpPath = {
+  post: {
+    tags: ['Accounts'],
+    summary: 'Create account API',
+    description: 'This route can be executed by any user',
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/signUpParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success'
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
